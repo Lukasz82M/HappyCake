83b2d5a6a5d5d030e50a4fd00a2aa3f1d202a5a7
lukasmalcherczyk <lukasadammal@gmail.com>
2022-09-26 12:06:26 +0200

    Edit login firebase stand 26.09.22

--- app/src/main/java/CakeAdapter.kt
+++ app/src/main/java/CakeAdapter.kt
@@ -2,10 +2,11 @@
 
 
 import androidx.recyclerview.widget.RecyclerView
+import de.syntaxinstitut.happycake.data.model.Cake
 
 /**
  * Diese Klasse organisiert mithilfe der ViewHolder Klasse das Recycling
  */
 abstract class CakeAdapter(
-    private val dataset: List<Item>
-) : RecyclerView.Adapter<CakeAdapter.recyclerViewProdukte>() {}
+    private val dataset: List<Cake>
+) : RecyclerView.Adapter<CakeAdapter.ItemViewHolder>() {}
