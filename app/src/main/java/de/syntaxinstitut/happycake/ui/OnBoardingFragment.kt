398a80e2729c9979e6837c269616a3720d0fe129
lukasmalcherczyk <lukasadammal@gmail.com>
2022-10-12 14:16:00 +0200

    Edit login firebase Login SignUp PLUS Kommentare signUp Screen Adapter stand 12.10.2022 CakeApiService.kt CakeRepository.kt MainViewModel.kt navGraph RecyclerView
    LÄUFT

--- app/src/main/java/de/syntaxinstitut/happycake/ui/OnBoardingFragment.kt
+++ app/src/main/java/de/syntaxinstitut/happycake/ui/OnBoardingFragment.kt
@@ -1,16 +1,17 @@
 package de.syntaxinstitut.happycake.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import de.syntaxinstitut.happycake.R
+import de.syntaxinstitut.happycake.databinding.FragmentOnBoardingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +19,21 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class OnBoardingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentOnBoardingBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_on_boarding,container, false)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            findNavController().navigate(OnBoardingFragmentDirections.actionOnBoardingFragmentToLoginFragment())
+        },5000)
+            return  binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OnBoardingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OnBoardingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
