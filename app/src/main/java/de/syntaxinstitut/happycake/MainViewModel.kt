1d9b64511f0ffa142ae375142683066ea0fe0da4
lukasmalcherczyk <lukasadammal@gmail.com>
2022-12-16 11:45:50 +0100

    relodet 15.12.22 pimpMyApp mit Datenbank läuft LÄUFT

--- app/src/main/java/de/syntaxinstitut/happycake/MainViewModel.kt
+++ app/src/main/java/de/syntaxinstitut/happycake/MainViewModel.kt
@@ -12,16 +12,18 @@ import kotlinx.coroutines.launch
 import java.lang.Exception
 
 private const val TAG = "MainViewModel"
+
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     enum class ApiStatus { LOADING, ERROR, DONE }
-//    private val datasource = Datasource()
+
+    //    private val datasource = Datasource()
     private val database = getDatabase(application)
     private val repository = CakeRepository(CakeApi, database)
 
     private val _loading = MutableLiveData<ApiStatus>()
     val loading: LiveData<ApiStatus>
-    get() = _loading
+        get() = _loading
 
     val cakes = repository.cakeList
 
@@ -37,28 +39,25 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     fun printImageUri(uri: String) {
         Log.d(TAG, uri)
     }
+
+
     fun loadData() {
         viewModelScope.launch {
-            repository.getCake()
-        }
-
-        fun loadData() {
-            viewModelScope.launch {
-                _loading.value = ApiStatus.LOADING
-                try {
-                    repository.getCake()
+            _loading.value = ApiStatus.LOADING
+            try {
+                repository.getCake()
+                _loading.value = ApiStatus.DONE
+            } catch (e: Exception) {
+                Log.e(TAG, "Error loading Data $e")
+                if (cakes.value.isNullOrEmpty()) {
+                    _loading.value = ApiStatus.ERROR
+                } else {
                     _loading.value = ApiStatus.DONE
-                } catch (e: Exception) {
-                    Log.e(TAG, "Error loading Data $e")
-                    if (cakes.value.isNullOrEmpty()) {
-                        _loading.value = ApiStatus.ERROR
-                    } else {
-                        _loading.value = ApiStatus.DONE
 
-                    }
                 }
             }
         }
     }
+
 }
 
